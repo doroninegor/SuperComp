2d2b1f7ecf066b10e9703553fec61eb8b61a6a2f
doroninegor <donineg2015@yandex.ru>
2021-12-16 16:27:54 +0300

    LR9

---
+++ src/LR9/MyRunnable.java
@@ -0,0 +1,21 @@
+package LR9;
+
+public class MyRunnable implements Runnable {
+    public void run() {
+        for (int i = 0; i < 100; i++)
+            if (i % 10 == 0) {
+                System.out.println(Thread.currentThread().getName() + ": " + i);
+                try {
+                    Thread.sleep(i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+    }
+
+    public static void main(String[] msi) {
+        MyRunnable thread = new MyRunnable();
+        for (int i = 0; i < 3; i++)
+            new Thread(thread).start();
+    }
+}
---
+++ src/LR9/MyThread.java
@@ -0,0 +1,24 @@
+package LR9;
+
+public class MyThread extends Thread{
+    StringBuilder sb;
+
+    public MyThread(StringBuilder sb) {
+        this.sb = sb;
+    }
+
+    public void run() {
+        synchronized (sb) {
+            for (int i = 0; i < 100; ++i)
+                System.out.print(i + "" + sb + "    ");
+            System.out.println("\n");
+            sb.setCharAt(0, (char) (sb.charAt(0) + 1));
+        }
+    }
+
+    public static void main(String[] msi) {
+        StringBuilder sb = new StringBuilder("a");
+        for (int i = 0; i < 3; i++)
+            new MyThread(sb).start();
+    }
+}
